fe73e5e69131bcc68db44c36fdbfe66a4d44651e
unfun <2512500628@qq.com>
2019-12-14 22:34:50 +0800

    Commit   to branch gh-pages

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-rm -r _site
+# rm -r _site
 echo '下载github pages 静态资源' && git clone -b gh-pages https://github.com/uhfun/Uhfun-Jekyll.git _site
-echo '删除除.git 外所有文件' && rm -rf _site/**/* || exit 0
+# echo '删除除.git 外所有文件' && rm -rf _site/**/* || exit 0
 echo '重新编译生成静态文件' && bundle install && bundle exec jekyll build
 cd _site
 echo '自定义域名' && echo 'uhfun.cn' > CNAME
