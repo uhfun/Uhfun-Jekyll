a9e5d95faf3cc54b4083f4622a6aeca132ee7d34
uhfun <2512500628@qq.com>
2020-05-09 07:39:26 +0000

    Commit 6ea4992846b4582bab8ef87dff046f1578a34f62 .. to branch gh-pages

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,24 @@
 # GH_USER_NAME          uhfun   
 # GH_PAGES_REPOSITORY   Uhfun-Jekyll
 
-echo '下载github pages 静态资源' && git clone -b ${GH_PAGES_BRANCH} git@github.com:${GH_USER_NAME}/${GH_PAGES_REPOSITORY}.git _site
+# master
+echo '添加posts分支内文章' && cd .. && git clone -b _posts git@github.com:${CI_REPO_NAME}.git _posts && ls _posts && mv _posts/*.md clone/_posts 
+echo '下载github pages 静态资源' && cd clone && git clone -b ${GH_PAGES_BRANCH} git@github.com:${CI_REPO_NAME}.git _site
 echo '删除除.git 外所有文件' && rm -rf _site/**/* || exit 0
 echo '重新编译生成静态文件' && bundle install && bundle exec jekyll build
-cd _site
-echo '自定义域名' && echo ${CUSTOM_DOAMIN} > CNAME
+cd _site && echo '自定义域名' && echo ${CUSTOM_DOAMIN} > CNAME
+git config --global user.email ${GH_USER_EMAIL}
+git config --global user.name ${GH_USER_NAME}
+git add .
+git commit -m "Commit ${CI_COMMIT_ID} ${CI_COMMIT_MESSAGE} to branch ${GH_PAGES_BRANCH}" && git push origin ${GH_PAGES_BRANCH}
+
+#_posts
+cd .. && echo '下载github pages 源文件' && git clone -b master git@github.com:${CI_REPO_NAME}.git _source && cd _source
+echo '添加posts分支内文章' && mv ../clone/* _posts
+echo '下载github pages 静态资源' && git clone -b ${GH_PAGES_BRANCH} git@github.com:${CI_REPO_NAME}.git _site
+echo '删除除.git 外所有文件' && rm -rf _site/**/* || exit 0
+echo '重新编译生成静态文件' && bundle install && bundle exec jekyll build
+cd _site && echo '自定义域名' && echo ${CUSTOM_DOAMIN} > CNAME
 git config --global user.email ${GH_USER_EMAIL}
 git config --global user.name ${GH_USER_NAME}
 git add .
