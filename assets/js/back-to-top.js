59e2275a82f8509a73e00181a1075c2a919506ad
fuhangbo <mynameisfhb@gmail.com>
2020-05-16 17:36:41 +0800

    ..

--- assets/js/back-to-top.js
+++ assets/js/back-to-top.js
@@ -1,1 +1,23 @@
-const maxHeight=400;window.onscroll=()=>{$('.back-to-top').css("opacity",document.documentElement.scrollTop>maxHeight||document.body.scrollTop>maxHeight?1:0)};$(".back-to-top").click(()=>{window.scrollTo({top:0,behavior:'smooth'})});
+const maxHeight = 400;
+var ticking = false; // rAF 触发锁
+ 
+function onScroll(){
+  if(!ticking) {
+    requestAnimationFrame(realFunc);
+    ticking = true;
+  }
+}
+ 
+function realFunc(){    
+    $('.back-to-top').css("opacity", document.documentElement.scrollTop > maxHeight || document.body.scrollTop > maxHeight ? 1 : 0)
+    ticking = false;
+}
+// 滚动事件监听
+window.addEventListener('scroll', onScroll, false);
+$(document).on('click', '.back-to-top', function() {
+    $(this).addClass('to-top')
+    window.scrollTo({
+		top: 0,
+		behavior: 'smooth'
+    })    
+})
--- assets/js/toc.js
+++ assets/js/toc.js
@@ -20,9 +20,9 @@
       });
     }
 
-    function createLink (header) {
+    function createLink (header, i) {
       var innerText = (header.textContent === undefined) ? header.innerText : header.textContent;
-      return "<a class='toc-link' href='#" + fixedEncodeURIComponent(header.id) + "'>" + innerText + "</a>";
+      return "<a class='toc-link' data-toc-link-index="+ i +" href='#" + fixedEncodeURIComponent(header.id) + "'>" + innerText + "</a>";
     }
 
     var headers = $(settings.headers).filter(function() {
@@ -60,18 +60,18 @@
         window.location.hash = this.id;
     })
     .addClass('clickable-header')
-    .each(function(_, header) {
+    .each(function(index, header) {
       this_level = get_level(header);
       if (this_level === highest_level) {
         $(header).addClass('top-level-header');
       }
       if (this_level === level) // same level as before; same indenting
-        html += "<li class=\"" + settings.classes.item + "\">" + createLink(header);
+        html += "<li class=\"" + settings.classes.item + "\">" + createLink(header, index);
       else if (this_level <= level){ // higher level than before; end parent ol
         for(var i = this_level; i < level; i++) {
           html += "</li></"+settings.listType+">"
         }
-        html += "<li class=\"" + settings.classes.item + "\">" + createLink(header);
+        html += "<li class=\"" + settings.classes.item + "\">" + createLink(header, index);
       }
       else if (this_level > level) { // lower level than before; expand the previous to contain a ol
         for(i = this_level; i > level; i--) {
@@ -79,7 +79,7 @@
                   "<" + settings.listType + " class=\"toc-sub-ol toc-ol" + settings.classes.list +"\">" +
                   "<li class=\"" + settings.classes.item + "\">"
         }
-        html += createLink(header);
+        html += createLink(header, index);
       }
       level = this_level; // update for the next one
     });
@@ -90,8 +90,9 @@
   var headerOffsets = (tocHeight) => {
     var titles = []
     $('.clickable-header').each((i, h) => {
-      var $h = $(h);
+      var $h = $(h);      
       titles.push({
+        "index" : i,        
         "href": '#' + encodeURI($h.attr('id')),
         "offsetTop": $h.offset().top - tocHeight        
       });
@@ -109,17 +110,6 @@
     }      
   });
 
-  var nearestHeader = function(scrollS, min, array) {
-    var nearest = array[0];
-    array.forEach((item,index,arr) => {
-      if (Math.abs(item.offsetTop - scrollS) < Math.abs(min)) {
-        nearest = item;
-        min = item.offsetTop - scrollS;
-      }          
-    })
-    return nearest;    
-  }
-
   var openLi = function($li) {
     if (!$li.hasClass('toc-open')) {
       closeLi($li.siblings('.toc-open'));
@@ -134,10 +124,41 @@
     $li.children('.toc-sub-ol').slideToggle(240);
     $li.children('.show-sub').children('svg').addClass('fa-caret-up').removeClass('fa-caret-down');           
   }
-
+  
   var windowTop=0, $toc = $('#toc'), titles = [];
+  var tocCheckedTitle = {};
+  function nearestTitle(scrollS) {
+    var nearestItem, min = 9999;
+    if (JSON.stringify(tocCheckedTitle) == '{}') {      
+      titles.forEach((item,index,arr) => {    
+        if (Math.abs(item.offsetTop - scrollS) < Math.abs(min)) {
+          nearestItem = item;
+          min = item.offsetTop - scrollS;
+        }          
+      })
+      tocCheckedTitle = nearestItem;
+    } else {
+      min = tocCheckedTitle.offsetTop - scrollS;
+      for (i = tocCheckedTitle.index-1; i >= 0; i--) { 
+        var newValue = Math.abs(titles[i].offsetTop - scrollS), oldValue = Math.abs(tocCheckedTitle.offsetTop - scrollS);
+        if (newValue < oldValue && newValue < min) {
+          min = newValue;
+          tocCheckedTitle = titles[i];
+        }
+        if (newValue > min) {
+          tocCheckedTitle = titles[i+1];
+          return
+        }      
+      }
+    }    
+  }
+
+  $(document).on('click', '.toc-link', function() {
+    tocCheckedTitle = titles[$(this).data("toc-link-index")]
+  });
+
   $(window).scroll(() => {
-    var scrollS = $(this).scrollTop();  
+    var scrollS = $(this).scrollTop(), $checkedA;  
     let h = $toc.outerHeight();    
     if ($toc.html() != '') {   
       if (titles.length == 0) {
@@ -152,14 +173,17 @@
           }          
         }
         if ($toc.css('position') == 'fixed' && scrollS >= windowTop && $toc.is(":visible")) {
+          $('.back-to-top').removeClass('to-top')
+          tocCheckedTitle = {}
           $toc.fadeOut();
         } 
         if ($toc.css('position') == 'fixed' && scrollS < windowTop && !$toc.is(":visible") ) {
           $toc.fadeIn()
         }       
-        if (scrollS < windowTop) {   
-          var $checkedA = $("[href='"+nearestHeader(scrollS, 99999, titles).href+"']");
-          $('.checked-a').removeClass('checked-a');
+        if (scrollS < windowTop && !$(".back-to-top").hasClass('to-top')) {             
+          nearestTitle(scrollS);          
+          $('.checked-a').removeClass('checked-a')      
+          $checkedA = $("[href='"+ tocCheckedTitle.href +"']");
           $checkedA.addClass('checked-a')
           var $li = $checkedA.parent();      
           if ($li.children('.toc-sub-ol').length == 0) {
